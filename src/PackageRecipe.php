3e3e18ae1ca5ee9ab600271c1ee823dbaa3cc1d8
k0d3r1s <k0d3r1s@gmail.com>
2023-02-22 21:16:26 +0200

    fix: missing methods

--- src/PackageRecipe.php
+++ src/PackageRecipe.php
@@ -16,4 +16,12 @@ final class PackageRecipe implements PluginInterface
         $this->composer = $composer;
         $this->io = $io;
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
